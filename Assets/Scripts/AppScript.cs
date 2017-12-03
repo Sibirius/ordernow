03854afe0292a70dde37f93b93d484e865ed6403
Sibi <sebastian.muehr@gmail.com>
2017-12-03 01:28:33 +0100

    added requirements to friends

--- Assets/Scripts/AppScript.cs
+++ Assets/Scripts/AppScript.cs
@@ -6,7 +6,7 @@ public class AppScript : MonoBehaviour {
 
     public static AppScript instance;
 
-	public Restaurant[] restaurants = {
+	public static Restaurant[] restaurants = {
 		new Restaurant("Marios", "Pizza"), 
 		new Restaurant("Marios", "Pizza"), 
 		new Restaurant("Marios", "Pizza"), 
@@ -23,22 +23,28 @@ public class AppScript : MonoBehaviour {
 		new Restaurant("Luigis", "More pizza")
 	};
 
-	public Friend[] friends = {
+	public static Friend[] friends = {
 		new Friend("Ross"), new Friend("Pikachu"), new Friend("Barb")
 	};
 
 	public GameObject restaurantButton;
 	public GameObject restaurantButtonsContainer;
     public GameObject profilePage;
+    public GameObject friendPrefab;
 
 	// Use this for initialization
 	void Start () {
         instance = this;
 		foreach (Restaurant restaurant in restaurants) {
-			GameObject newButton = Instantiate (restaurantButton, restaurantButtonsContainer.transform) as GameObject;
+			GameObject newButton = Instantiate (restaurantButton, restaurantButtonsContainer.transform);
 			RestaurantButtonScript newScript = newButton.GetComponent<RestaurantButtonScript> ();
 			newScript.SetRestaurant(restaurant);
 		}
+
+        foreach (Friend friend in friends) {
+            //TODO position friends correctly in the scene
+            //GameObject newFriend = Instantiate(friendPrefab);
+        }
 	}
 	
 	// Update is called once per frame
@@ -69,9 +75,37 @@ public class Restaurant {
 public class Friend {
 	public string name;
 	public int satisfaction;
+    public Ingredient requirement;
 
 	public Friend(string name) {
 		this.name = name;
 		this.satisfaction = 100;
+        this.requirement = Utils.randomIngredient();
+        Debug.Log(name+" likes "+requirement);
 	}
+}
+
+public enum Ingredient
+{
+    Peperoni, Tuna, Cheese
+}
+
+public class Utils
+{
+
+    private static System.Random random;
+
+    private static System.Random getRandom()
+    {
+        if (random == null) {
+            random = new System.Random();
+        }
+        return random;
+    }
+
+    public static Ingredient randomIngredient()
+    {
+        System.Array ingredients = System.Enum.GetValues(typeof(Ingredient));
+        return (Ingredient)ingredients.GetValue(getRandom().Next(ingredients.Length));
+    }
 }
